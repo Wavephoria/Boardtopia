fbdb2083580d6700f1e8b730aa112eb814707b7a
Jens Gustafsson <118354433+Wavephoria@users.noreply.github.com>
2023-03-24 11:59:38 +0100

    Delete Boredtopia/BoredtopiaServerSide directory

--- Boredtopia/BoredtopiaServerSide/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
-var app = builder.Build();
-
-app.UseHttpsRedirection();
-app.UseRouting();
-app.UseEndpoints(endpoints => endpoints.MapControllers());
-
-app.Run();
