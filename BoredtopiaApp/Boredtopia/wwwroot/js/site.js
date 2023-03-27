345013af10fc54e6150f42904abc0f66f86a3a3f
Emilia Engberg <engberg.emilia@gmail.com>
2023-03-27 10:37:40 +0200

    Add row to gameboard and basic styling for development purposes

--- BoredtopiaApp/Boredtopia/wwwroot/js/site.js
+++ BoredtopiaApp/Boredtopia/wwwroot/js/site.js
@@ -8,16 +8,18 @@ const NUMBER_OF_GUESSES = 6;
 let currentGuess = '';
 let currentNum = 0;
 
-
 // Word during development
 const word = "REACT";
 
 
-
-
-
 // Create game board
-
+function createGameBoard() {
+    for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+        const wordBox = document.createElement('div');
+        wordBox.classList.add('word-box');
+        gameBoard.appendChild(wordBox);
+    }
+}
 
 
 
@@ -30,3 +32,4 @@ const word = "REACT";
 
 
 // Init game
+createGameBoard();
