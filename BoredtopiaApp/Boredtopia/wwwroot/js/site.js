974a519f64a9e2b652de3eb518ace2c29d3e69fc
Emilia Engberg <engberg.emilia@gmail.com>
2023-03-27 10:09:22 +0200

    Add initial variables and query selector

--- BoredtopiaApp/Boredtopia/wwwroot/js/site.js
+++ BoredtopiaApp/Boredtopia/wwwroot/js/site.js
@@ -1,5 +1,16 @@
 ﻿
+// Queryselectors
+const gameBoard = document.querySelector('.game-board');
+
 // Add game variables
+const WORD_LENGTH = 6;
+const NUMBER_OF_GUESSES = 6;
+let currentGuess = '';
+let currentNum = 0;
+
+
+// Word during development
+const word = "REACT";
 
 
 
