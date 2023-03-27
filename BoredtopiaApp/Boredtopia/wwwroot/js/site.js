2c0964ba4fc793329590234cc6177245620fd0a6
Emilia Engberg <engberg.emilia@gmail.com>
2023-03-27 13:58:36 +0200

    Add fuctionality to move to next row. Also check that number of guesses is not exceeded

--- BoredtopiaApp/Boredtopia/wwwroot/js/site.js
+++ BoredtopiaApp/Boredtopia/wwwroot/js/site.js
@@ -34,14 +34,17 @@ function createGameBoard() {
 
 // Add event listener
 document.addEventListener('keyup', (e) => {
-    const row = document.querySelector('[data-row="row1"]');
-    if (regEx.test(e.key) && currentGuess.length < WORD_LENGTH) {
-        row.children[currentGuess.length].textContent = e.key.toUpperCase();
-        currentGuess += e.key;
-    } else if (e.key === 'Enter' && currentGuess.length === WORD_LENGTH) {
-        console.log(e.key);
-    } else if (e.key === 'Backspace') {
-        console.log(e.key);
+    if (currentNum < NUMBER_OF_GUESSES) {
+        const row = document.querySelector(`[data-row=row${currentNum + 1}]`);
+        if (regEx.test(e.key) && currentGuess.length < WORD_LENGTH) {
+            row.children[currentGuess.length].textContent = e.key.toUpperCase();
+            currentGuess += e.key;
+        } else if (e.key === 'Enter' && currentGuess.length === WORD_LENGTH) {
+            currentGuess = '';
+            currentNum++;
+        } else if (e.key === 'Backspace') {
+            console.log(e.key);
+            }
     }
 });
 
