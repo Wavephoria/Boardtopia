393f6f324187b3617b598f8169cefa550fcff845
Rasmus Nilsson <rasmus.nilsson@brightstraining.com>
2023-03-28 16:36:50 +0200

    Added rock paper scissor game

---
+++ BoredtopiaApp/Boredtopia/wwwroot/js/rockpaperscissor.js
@@ -0,0 +1,32 @@
+﻿const options = ["rock", "paper", "scissors"];
+
+function computerPlay() {
+    return options[Math.floor(Math.random() * options.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return "You win! " + playerSelection + " beats " + computerSelection;
+    } else {
+        return "You lose! " + computerSelection + " beats " + playerSelection;
+    }
+}
+
+const images = document.querySelectorAll("img");
+images.forEach((image) => {
+    image.addEventListener("click", () => {
+        const playerSelection = image.id;
+        const computerSelection = computerPlay();
+        const result = playRound(playerSelection, computerSelection);
+        document.getElementById("result").textContent = result;
+    });
+});
