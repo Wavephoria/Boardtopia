47e96ed404591258be4fefd441dedeaa17e89b80
Rasmus Nilsson <rasmus.nilsson@brightstraining.com>
2023-03-30 14:29:14 +0200

    Rock paper scissors updated

--- BoredtopiaApp/Boredtopia/wwwroot/js/rockpaperscissor.js
+++ BoredtopiaApp/Boredtopia/wwwroot/js/rockpaperscissor.js
@@ -1,5 +1,11 @@
 ﻿const options = ["rock", "paper", "scissors"];
 
+let rockWins = 0;
+let paperWins = 0;
+let scissorsWins = 0;
+let currentWins = 0;
+let highScore = 0;
+
 function computerPlay() {
     return options[Math.floor(Math.random() * options.length)];
 }
@@ -9,14 +15,31 @@ function playRound(playerSelection, computerSelection) {
     computerSelection = computerSelection.toLowerCase();
 
     if (playerSelection === computerSelection) {
+        currentWins = 0;
         return "It's a tie!";
     } else if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
     ) {
+        switch (playerSelection) {
+            case "rock":
+                rockWins++;
+                break;
+            case "paper":
+                paperWins++;
+                break;
+            case "scissors":
+                scissorsWins++;
+                break;
+        }
+        currentWins++;
+        if (currentWins > highScore) {
+            highScore = currentWins;
+        }
         return "You win! " + playerSelection + " beats " + computerSelection;
     } else {
+        currentWins = 0;
         return "You lose! " + computerSelection + " beats " + playerSelection;
     }
 }
@@ -28,5 +51,10 @@ images.forEach((image) => {
         const computerSelection = computerPlay();
         const result = playRound(playerSelection, computerSelection);
         document.getElementById("result").textContent = result;
+        document.getElementById("rock-wins").textContent = rockWins;
+        document.getElementById("paper-wins").textContent = paperWins;
+        document.getElementById("scissors-wins").textContent = scissorsWins;
+        document.getElementById("current-wins-count").textContent = currentWins;
+        document.getElementById("high-score-count").textContent = highScore;
     });
 });
