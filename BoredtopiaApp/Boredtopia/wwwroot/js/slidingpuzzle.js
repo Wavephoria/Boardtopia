3bfa86ef19ebc0ebc6d1d0620ec9dfbfaf419cde
Emilia Engberg <engberg.emilia@gmail.com>
2023-03-30 10:52:11 +0200

    Create sliding puzzle game board

--- BoredtopiaApp/Boredtopia/wwwroot/js/slidingpuzzle.js
+++ BoredtopiaApp/Boredtopia/wwwroot/js/slidingpuzzle.js
@@ -1,10 +1,20 @@
 ﻿// create game variables
-
+const gameBoard = document.querySelector('.puzzle-board');
+const rowSize = 3;
+const numberOfTiles = rowSize ** 2;
 
 // create board
 // - add tiles
 // - add class to tile (tile1, tile2, etc.)
 // - add eventlistener to each tile (move on click)
+
+function createBoard() {
+    for (let i = 1; i <= numberOfTiles; i++) {
+        const tile = document.createElement('div');
+        tile.classList.add(`tile${i}`);
+        gameBoard.appendChild(tile);
+    }
+}
 
 
 
@@ -26,7 +36,15 @@
 // - call shuffle tiles function
 // - check that it's not in win state
 
+function startGame() {
+    createBoard();
+}
+
 
 // check if player has won (puzzle completed)
 // - check if puzzle board is in win state
 // - if won end game with win message
+
+
+
+startGame();
