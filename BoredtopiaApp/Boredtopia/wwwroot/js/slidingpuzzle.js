984d354f3efaac822ffe335de2380cc3e717e76f
Emilia Engberg <engberg.emilia@gmail.com>
2023-03-30 10:32:19 +0200

    Initial comments and stuff for sliding puzzle game

--- BoredtopiaApp/Boredtopia/wwwroot/js/slidingpuzzle.js
+++ BoredtopiaApp/Boredtopia/wwwroot/js/slidingpuzzle.js
@@ -1,1 +1,32 @@
-﻿
+﻿// create game variables
+
+
+// create board
+// - add tiles
+// - add class to tile (tile1, tile2, etc.)
+// - add eventlistener to each tile (move on click)
+
+
+
+// shuffle tiles
+// - create random function
+// - shuffle tiles to random locations
+
+
+
+// move tile to empty spot
+// - check for empty spot
+// - move tile if it is next to empty spot
+// - call check if player has won function
+
+
+
+// start game
+// - call create board function
+// - call shuffle tiles function
+// - check that it's not in win state
+
+
+// check if player has won (puzzle completed)
+// - check if puzzle board is in win state
+// - if won end game with win message
